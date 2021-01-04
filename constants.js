96b262f611eeadfc9c82e7be069e53a82d78ff6e
Connor Brady <connorbrady@codox.eu>
2021-01-04 23:22:12 +0000

    UPDATED URL Const

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    GRAFANA_SOURCE_IPS: 'https://grafana.com/api/hosted-grafana/source-ips',
+    URL_GRAFANA_SOURCE_IPS: 'https://grafana.com/api/hosted-grafana/source-ips',
     SCOPES: [
         'https://www.googleapis.com/auth/cloud-platform'
     ],
