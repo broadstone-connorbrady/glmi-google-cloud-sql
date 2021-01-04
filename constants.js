e23ab0d837daaa49f3efbdf6cda7da3e86b5f971
Connor Brady <connorbrady@codox.eu>
2021-01-04 22:58:13 +0000

    ADDED GCP API Scopes

--- constants.js
+++ constants.js
@@ -1,3 +1,7 @@
 module.exports = Object.freeze({
     GRAFANA_SOURCE_IPS: 'https://grafana.com/api/hosted-grafana/source-ips',
+    SCOPES: [
+        'https://www.googleapis.com/auth/cloud-platform'
+    ],
+
 });
