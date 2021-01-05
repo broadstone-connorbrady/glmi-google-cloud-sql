11740132fae1b624f7316b76abde8d012a7ea38f
Connor Brady <connorbrady@codox.eu>
2021-01-05 21:55:54 +0000

    WIP Adding unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,13 @@
 const assert = require('assert');
 
 // Constants
-const constants = require('../constants');
+const constants = require('constants.test');
+
+// Main file
+const index = require('../index');
+
+describe('Grafana - Get Source IPs', async () => {
+    const ips = await index.getGrafanaSourceIPs();
+
+    console.log(ips);
+})
