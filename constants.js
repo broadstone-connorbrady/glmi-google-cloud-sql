cec5c0f759f28b1c4b4dfcdd7f6a1ac41a40da97
Connor Brady <connor.brady@broadstone-engage.com>
2021-01-10 01:12:16 +0000

    REMOVED Constants file

--- constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = Object.freeze({
-    URL_GRAFANA_SOURCE_IPS: 'https://grafana.com/api/hosted-grafana/source-ips',
-    SCOPES: [
-        'https://www.googleapis.com/auth/cloud-platform'
-    ],
-
-});
